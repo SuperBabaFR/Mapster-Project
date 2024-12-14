9007f610b90968e9b177b1c63f943c171bb26f39
SuperBabaFR <78614041+SuperBabaFR@users.noreply.github.com>
2024-12-14 01:09:39 -0400

    Les posts c'est op

--- Mapster/www/js/index.js
+++ Mapster/www/js/index.js
@@ -1,14 +1,12 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 const date = new Date();
-console.log(date.toLocaleString('en-US', { timeZone: 'America/New_York' })); // New York time
-console.log(date.toLocaleString('en-GB', { timeZone: 'Europe/London' })); // London time
 
-data = {
+const data = {
     "liste":[
         {
             "id":1,
-            "photo":"xxx",
+            "photo":"https://wallpapercave.com/wp/wp7205972.jpg",
             "date": date.toJSON(),
             "longitude":16.224012176700928,
             "latitude":-61.528382151240194,
@@ -17,7 +15,7 @@ data = {
             "Maper":{
                 "idMapper":1,
                 "pseudoMapper":"Goku",
-                "photoProfil":"xx"
+                "photoProfil":"https://avatarfiles.alphacoders.com/243/thumb-1920-243239.jpg"
             }
         },
         {
@@ -37,7 +35,7 @@ data = {
     ]
 }
 
-
+showListPosts();
 
 function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
@@ -48,27 +46,58 @@ function onDeviceReady() {
 
 function showListPosts () {
 
-    const allPosts = data;
+    const allPosts = data.liste;
 
     const postList = document.getElementById('postsList');
 
-    for (const postdata in allPosts) {
+    for (let i = 0; i < allPosts.length; i++) {
+        const postdata = allPosts[i];
 
         const post = document.createElement('div');
-        post.id = "post " + postdata.id;
-        post.className = 'post';
+            post.id = "post " + postdata.id;
+            post.className = 'post';
 
-        const headPost = document.createElement('div'); headPost.className = 'head-post';
+        const headPost = document.createElement('div');
+            headPost.className = 'head-post';
 
-        const photoprofil = document.createElement('img'); photoprofil.className = 'head-post';
-        const pseudo = document.createElement('p');
-        const distance = document.createElement('p');
+            const photoprofil = document.createElement('img');
+            photoprofil.className = 'pp';
+            photoprofil.src = postdata.Maper.photoProfil;
 
-        const image = document.createElement('img');
+            const infosMapster = document.createElement('div');
+
+            const pseudo = document.createElement('p');
+            pseudo.className = 'pseudo';
+            pseudo.innerHTML = postdata.Maper.pseudoMapper;
+
+            const distance = document.createElement('p');
+            distance.className = 'distance';
+            distance.innerHTML = "à " + postdata.distanceUser + " d'ici";
+
+            infosMapster.appendChild(pseudo);
+            infosMapster.appendChild(distance);
+
+        headPost.appendChild(photoprofil);
+        headPost.appendChild(infosMapster);
+
+        const image_post = document.createElement('img');
+        image_post.className = 'img-post';
+        image_post.src = postdata.photo;
 
         const description = document.createElement('p');
+        description.className = 'description';
+        description.innerHTML = postdata.descriptions;
 
         const date = document.createElement('p');
+        date.className = 'date';
+        date.innerHTML = postdata.date;
+
+        post.appendChild(headPost);
+        post.appendChild(image_post);
+        post.appendChild(description);
+        post.appendChild(date);
+
+        postList.appendChild(post);
 
     }
 }
