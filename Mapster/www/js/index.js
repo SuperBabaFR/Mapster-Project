d8eaecaccb2f2d7aa712a438ea83ee5781027b27
SuperBabaFR <78614041+SuperBabaFR@users.noreply.github.com>
2024-12-14 00:20:20 -0400

    premiers tests

--- Mapster/www/js/index.js
+++ Mapster/www/js/index.js
@@ -1,29 +1,74 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+const date = new Date();
+console.log(date.toLocaleString('en-US', { timeZone: 'America/New_York' })); // New York time
+console.log(date.toLocaleString('en-GB', { timeZone: 'Europe/London' })); // London time
 
+data = {
+    "liste":[
+        {
+            "id":1,
+            "photo":"xxx",
+            "date": date.toJSON(),
+            "longitude":16.224012176700928,
+            "latitude":-61.528382151240194,
+            "distanceUser":"200m",
+            "descriptions":"Mon plus gros Ops",
+            "Maper":{
+                "idMapper":1,
+                "pseudoMapper":"Goku",
+                "photoProfil":"xx"
+            }
+        },
+        {
+            "id":2,
+            "photo":"xxx",
+            "date":date.toJSON(),
+            "longitude":16.224012176700928,
+            "latitude":-61.528382151240194,
+            "distanceUser":"250m",
+            "descriptions":"Enfin je me suis débarrassé de ce singe",
+            "Maper":{
+                "idMapper":2,
+                "pseudoMapper":"Freezer",
+                "photoProfil":"xx"
+            }
+        }
+    ]
+}
+
+
+
+function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+    showListPosts()
+}
+
+
+function showListPosts () {
+
+    const allPosts = data;
+
+    const postList = document.getElementById('postsList');
+
+    for (const postdata in allPosts) {
+
+        const post = document.createElement('div');
+        post.id = "post " + postdata.id;
+        post.className = 'post';
+
+        const headPost = document.createElement('div'); headPost.className = 'head-post';
+
+        const photoprofil = document.createElement('img'); photoprofil.className = 'head-post';
+        const pseudo = document.createElement('p');
+        const distance = document.createElement('p');
+
+        const image = document.createElement('img');
+
+        const description = document.createElement('p');
+
+        const date = document.createElement('p');
+
+    }
 }
