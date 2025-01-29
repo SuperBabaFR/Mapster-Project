4ab0c7f0730b029f3c5c877c784d05b337b67691
YohannC971 <132513037+YohannC971@users.noreply.github.com>
2025-01-29 08:26:28 -0400

    Update db_config.php

--- PosterUnePhoto/Backend/db_config.php
+++ PosterUnePhoto/Backend/db_config.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_HOST', 'localhost'); 
-define('DB_NAME', 'db_test');   // Le nom de la base de données
-define('DB_USER', 'root');      // L'utilisateur par défaut de UwAmp est "root"
+define('DB_NAME', 'db_mapster');   // Le nom de la base de données
+define('DB_USER', 'root');
 define('DB_PASSWORD', 'root');    
 ?>
